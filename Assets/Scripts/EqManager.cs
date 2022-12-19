98b291e3661ce1c552955c86d4a303446f2f992c
kacperski36 <kacperski36@wp.pl>
2022-12-19 17:06:28 +0100

    collecting items

--- Assets/Scripts/EqManager.cs
+++ Assets/Scripts/EqManager.cs
@@ -3,10 +3,15 @@ using UnityEngine.UI;
 
 public class EqManager : MonoBehaviour
 {
+    public static EqManager instance;
     public Slot[] slots;
     public GameObject itemPrefab;
     int focusedSlot = -1;
     public Hunger hunger;
+    private void Awake()
+    {
+        instance = this;
+    }
     private void Start()
     {
         ChangeFocusOnSlot(0);
@@ -66,21 +71,26 @@ public class EqManager : MonoBehaviour
         ItemInInventory slotValue = slot.GetComponentInChildren<ItemInInventory>();
         if (slotValue != null)
         {
-            if (slotValue.item.itemType == itemType.Food)
-            {
-                hunger.Eating(slotValue.item.actionValue);
-                slotValue.count--;
-                if (slotValue.count <= 0)
-                {
-                    Destroy(slotValue.gameObject);
-                }
-
-            }
-
+            EatAndDestroy(slotValue);
             return slotValue.item;
         }
         return null;
     }
+
+    private void EatAndDestroy(ItemInInventory slotValue)
+    {
+        if (slotValue.item.itemType == itemType.Food)
+        {
+            hunger.Eating(slotValue.item.actionValue);
+            slotValue.count--;
+            if (slotValue.count <= 0)
+            {
+                Destroy(slotValue.gameObject);
+            }
+
+        }
+    }
+
     public void useItem()
     {
         if (Input.GetMouseButtonDown(0))
--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
+    [SerializeField] public SphereCollider itemCollider;
+    [SerializeField] public float movespeed;
+
     public Item item;
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            bool isAdded = EqManager.instance.AddItem(item);
+            if (isAdded) { Collect(); }
+            Debug.Log(other.gameObject.tag);
+
+        }
+    }
+    private void Collect()
+    {
+        Destroy(itemCollider);
+        Destroy(gameObject);
+        Debug.Log("Collected");
+    }
 }
