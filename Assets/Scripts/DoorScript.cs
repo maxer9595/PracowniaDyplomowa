ec0afe545e9964395462d1f95bf12e962f4f85a1
kacperski36 <kacperski36@wp.pl>
2023-01-17 17:52:53 +0100

    bug fix

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -9,17 +9,25 @@ public class DoorScript : MonoBehaviour
     GameObject player;
     private bool opened = false;
     private Animator anim;
+    public bool isRotated;
+    Vector3 center = new Vector3(0, 0, 1);
 
     void Start()
     {
         player = EqManager.instance.player;
+        if (isRotated)
+        {
+            center = new Vector3(1, 0, 0);
+        }
     }
 
     void Update()
     {
-        distanceFromPlayer = Vector3.Distance(transform.position - new Vector3(0, 0, 1), player.transform.position);
+        distanceFromPlayer = Vector3.Distance(transform.position - center, player.transform.position);
         OpenAndClose();
+
     }
+
 
     private void OpenAndClose()
     {
@@ -37,6 +45,6 @@ public class DoorScript : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position - new Vector3(0, 0, 1), maxDistancefromPlayer);
+        Gizmos.DrawWireSphere(transform.position - center, maxDistancefromPlayer);
     }
 }
