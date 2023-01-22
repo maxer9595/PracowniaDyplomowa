737c0518369e44dd656993b49cad9d684a5a27c2
kacperski36 <kacperski36@wp.pl>
2023-01-22 19:18:08 +0100

    music fix

--- Assets/Scripts/SceneMenager.cs
+++ Assets/Scripts/SceneMenager.cs
@@ -3,23 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneMenager : MonoBehaviour
 {
+    public bool changeMusic = false;
     private void Start()
     {
         if (SceneManager.GetActiveScene().name == "DeathScreen" || SceneManager.GetActiveScene().name == "EndLevelScene")
         {
             Cursor.lockState = CursorLockMode.Confined;
         }
-         if (SceneManager.GetActiveScene().name == "MapScene")
+        if (SceneManager.GetActiveScene().name == "MapScene")
         {
             bgMusic.instance.GetComponent<AudioSource>().Pause();
         }
-        if (SceneManager.GetActiveScene().name == "MainMenu")
+        // if (SceneManager.GetActiveScene().name == "MainMenu" && dontChangeMusic)
+        // {
+        //     bgMusic.instance.GetComponent<AudioSource>().Play();
+        // }
+    }
+    public void ReturnToMenu(bool changeMusic = false)
+    {
+        if (!changeMusic)
         {
             bgMusic.instance.GetComponent<AudioSource>().Play();
         }
-    }
-    public void ReturnToMenu()
-    {
         SceneManager.LoadScene("MainMenu");
     }
     public void GoToMap()
--- Assets/Scripts/bgMusic.cs
+++ Assets/Scripts/bgMusic.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class bgMusic : MonoBehaviour
 {
     public static bgMusic instance;
- 
+
     void Awake()
     {
         if (instance != null)
